8a3b5d5d824d669fb2a833cdf8925f31bf2dc8df
Raymond Thurman <raymondthurman5@gmail.com>
2022-03-20 02:53:11 -0400

    removed outline from nav, changing animations

--- frontend/src/containers/Hero.jsx
+++ frontend/src/containers/Hero.jsx
@@ -1,22 +1,26 @@
-import { MotionWrapper } from "../wrapper";
+import { motion } from "framer-motion";
 import "../styles/Hero.css";
 
 const Hero = () => {
   return (
     <div id="home" className="hero min-h-screen">
       <div className="hero-content text-center">
-        <div className="max-w-md content">
+        <motion.div
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="max-w-md content"
+        >
           <h1 className="text-5xl font-bold">Welcome to Saiyan Prints</h1>
           <p className="py-6 text-xl">
             Send files to{" "}
-            <span className="text-primary">print@saiyanprints.com</span> for a
+            <span className="text-primary">quotes@saiyanprints.com</span> for a
             free quote!
           </p>
           <button className="btn btn-primary">Visit Shop</button>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
 };
 
-export default MotionWrapper(Hero);
+export default Hero;
