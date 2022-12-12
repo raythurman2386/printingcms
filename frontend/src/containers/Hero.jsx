a6060a7ab76ddf15dbfbdfed5d2a0782f450e21c
raythurman2386 <raymondthurman5@gmail.com>
2022-12-12 10:55:28 -0600

    updated link

--- frontend/src/containers/Hero.jsx
+++ frontend/src/containers/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           <h1 className="text-5xl font-bold">Welcome to Saiyan Prints</h1>
           <p className="py-6 text-xl">What can we print for you?</p>
           <a
-            href="https://saiyanprints.patternbyetsy.com/"
+            href="https://www.saiyanprints.store/"
             target="_blank"
             rel="noreferrer"
             className="btn btn-primary"
