2a1560848b894874abd7c8b2957f7fae3b6fa5f3
raythurman2386 <raymondthurman5@gmail.com>
2023-07-27 09:47:57 -0500

    google analytics added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import ReactGA from 'react-ga'
 import {
   About,
   Affiliates,
@@ -9,6 +11,9 @@ import {
 import { Layout } from "./wrapper";
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  })
   return (
     <>
       <Hero />
