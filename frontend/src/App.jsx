be4b23482ab751cf6a7486233ad2d95115389f3a
raythurman2386 <raymondthurman5@gmail.com>
2023-08-01 09:58:45 -0500

    updated deps, react-ga - react-ga4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import ReactGA from "react-ga";
 import {
   About,
   Affiliates,
@@ -11,9 +9,6 @@ import {
 import { Layout } from "./wrapper";
 
 const App = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  });
   return (
     <>
       <Hero />
